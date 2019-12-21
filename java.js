303b381d3ddb9310391848d64315e1d4857c5495
cyrian-banguis <55710257+cyrian-banguis@users.noreply.github.com>
2019-12-21 23:53:41 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,2 @@
+var boi = alert("Welcome to Mario's !!!");
+var fyre = prompt("Please rate our Pizza's between 1-10 if you have eaten them prior to your visit on our website or just press okay if you haven't !!!");
